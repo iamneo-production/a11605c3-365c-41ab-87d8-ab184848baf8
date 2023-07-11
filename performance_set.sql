05b954ee9666a610b280fa7f8ac160420b0a57ac
harsha529 <harshaunnamatla58@gmail.com>
2023-07-11 04:17:53 +0000

    question 1

---
+++ performance_set.sql
@@ -0,0 +1,12 @@
+create index idx_cnty_cutmr on sales(country,customername);
+
+
+/*1)----Total customer list who ordered from USA----*/
+select distinct(customername)
+from SALES
+where country='USA';
+/-----Performance result------/
+explain plan for select distinct(customername)
+from SALES
+where country='USA';
+select * from table(dbms_xplan.display);
