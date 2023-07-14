bac871c437dbded06b8dd1708647968cae3ed017
harsha529 <harshaunnamatla58@gmail.com>
2023-07-14 06:04:48 +0000

    updated set

--- performance_set.sql
+++ performance_set.sql
@@ -7,8 +7,13 @@ where country='USA'
 group by customername;
 
 /*3) total products orderd in the year 2003*/
-create index idx_product on sales(year_id,PRODUCTCODE);
+create index idx_product on sales(year_id,PRODUCTCODE,sales);
 select count(productcode)
 from sales
 where year_id=2003
 group by productcode;
+
+/*4) find the total price in the year 2005*/
+select sum(sales)
+from sales
+where year_id=2005;
