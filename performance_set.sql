805e5f644881d28272a570b306dbbc757effe55b
harsha529 <harshaunnamatla58@gmail.com>
2023-07-17 06:11:06 +0000

    performance set questions

--- performance_set.sql
+++ performance_set.sql
@@ -1,12 +1,16 @@
 
 /*1) total list of customers ordered from USA*/
-
 select distinct(customername)
 from sales
 where country='USA';
-/*3) total products orderd in the year 2003*/
 
-select count(distinct(productcode))
+/*2) Count the customers who have made purchases exceeding 5000*/
+select count(distinct(customername))
+from sales
+where sales>5000;
+
+/*3) total products orderd in the year 2003*/
+select count(productcode)
 from sales
 where year_id=2003
 group by productcode;
@@ -15,3 +19,8 @@ group by productcode;
 select sum(sales)
 from sales
 where year_id=2005;
+
+/*5) find the total sales price each year*/
+select year_id as Year,sum(sales) as Sales
+from sales
+group by year_id;
