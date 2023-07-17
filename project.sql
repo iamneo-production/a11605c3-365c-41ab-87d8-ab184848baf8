342aa776c667c04030ee845adcc227e856938cc8
Swathi1344 <gadirajuswathi1344@gmail.com>
2023-07-17 11:28:41 +0000

    project

--- project.sql
+++ project.sql
@@ -2,4 +2,8 @@
 select DISTINCT(customername) from sales
 where country='USA';
 
-/2nd question*/
+/*2nd question*/
+select distinct(count(customername)) from sales 
+where sales>5000;
+
+/*3rd question*/
