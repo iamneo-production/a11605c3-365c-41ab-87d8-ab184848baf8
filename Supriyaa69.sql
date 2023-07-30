42b75d9d3c5a9a99e7703cb0e30cc3a0562ac9e2
Supriyaa69 <20955A0520@iare.ac.in>
2023-07-30 13:14:32 +0000

    updated queries

---
+++ Supriyaa69.sql
@@ -0,0 +1,26 @@
+select * from sales;
+   
+--1).Query to find total list of customers ordered from USA
+
+select distinct(customername) from sales 
+where country='USA';
+
+--2).Query to count the number of customers who have made purchases exceeding 5000
+
+select count(distinct(customername)) as total_customers from sales 
+where sales>5000;
+
+--3).Query to find how many products are ordered in the year 2003
+
+select count(distinct(productcode)) as tota_products from sales 
+where year_id=2003;
+
+--4).Query to find total sale price in the year 2005
+
+select sum(sales) as total_sale_price from sales where year_id=2005;
+
+--5).Query to find total sale price each year
+
+select year_id,sum(sales) as total_sales from sales
+group by year_id
+order by year_id;
